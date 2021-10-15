420a94fb41da81e49cf6312b9f82f3d06cffd9d5
hyunji1223 <hyunji12230327@gmail.com>
2021-10-15 09:24:47 +0900

    2차 1차 개발넘김

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	$('.pop-btn').off().on('click', function(){
 		layerHandler(this);
 	});
+	selectPopup();
 
 	// toggle
 	$('.toggle-action > ul > li dt').off().on('click', function () {
@@ -35,8 +36,20 @@ $(document).ready(function(){
 		$(this).next().addClass('lab-top');
 	});
 	$('.ipt input').blur(function(){
-		if( $('.ipt input').val() == ''){
+		if($(this).val() == ''){
 			$(this).next().removeClass('lab-top');
+		}
+	});
+
+	//input
+	$('.ipt-idnum input').focus(function(){
+		$(this).parents('.ipt-box02').addClass('active');
+		$(this).parents('label').addClass('lab-top');
+	});
+	$('.ipt-idnum input').blur(function(){
+		$(this).parents('.ipt-box02').removeClass('active');
+		if( $(this).val() == ''){
+			$(this).parents('label').removeClass('lab-top');
 		}
 	});
 
@@ -64,5 +77,64 @@ function layerHandler(obj) {
 		idPop.fadeOut();
 		$('body').css('overflow-y','auto');
 		return false;
+	});
+};
+
+
+//select popup
+function selectPopup() {
+	var target = $('.ipt-sel');
+	
+	// 셀렉트 클릭 버튼 이벤트
+	$('.btn-sel').off().on('click', function() {
+		var targetSel = $(this).next();
+		var popTarget = $('.pop-sel');
+		var selectValue = targetSel.val();
+		var selTit = $(this).text();
+
+		var tmp = ''
+		targetSel.find('option').each(function(i) {
+			if(typeof $(this).attr('value') != 'undefined') {
+				tmp += '<li class="sel-radio">';
+				if($(this).attr('value') == selectValue) {
+					tmp += '	<input type="radio" name="sel_radio" id="sel_radio_' + i +'" value="' + $(this).attr('value') + '" checked />';
+				} else {
+					tmp += '	<input type="radio" name="sel_radio" id="sel_radio_' + i +'" value="' + $(this).attr('value') + '" />';
+				}
+				tmp += '	<label for="sel_radio_' + i +'">' + $(this).html() + '</label>';
+				tmp += '</li>';
+			}
+		});
+		popTarget.find('ul').html('');
+		popTarget.find('ul').html(tmp);
+		popTarget.find('.sel-head h1').text(selTit);
+		if(targetSel.attr('disabled') != 'disabled'){
+			popTarget.show();
+			$('body').css('overflow', 'hidden');
+		}
+		$('input[name="sel_radio"]').off().on('click', function() {
+			var radioValue = $('input[name="sel_radio"]:checked').val();
+			var oldVal = targetSel.val();
+			targetSel.val(radioValue);
+			targetSel.val(radioValue).attr("selected","selected");
+			var newVal = targetSel.val();
+			popTarget.hide();
+			$('body').css('overflow','auto');
+			
+			//select박스 onchange기능. select태그에 attribute를 change=펑션명 으로 사용. 
+			if( oldVal != newVal ){
+				var changeFn = targetSel.attr("change");
+				if( changeFn != null && typeof changeFn != "undefined" && changeFn != "" ){
+					var convertStringToFunction = window[changeFn];
+					convertStringToFunction();
+				}
+			}
+			
+		});
+		
+		$('.close-sel').off().on('click', function() {
+			popTarget.hide();
+			$('body').css('overflow','auto');
+		});
 	});
 };
